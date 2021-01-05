762341b0d79d5d8ceb73c3bbebe9166ae225254e
hylas-develop <74232110+hylas-develop@users.noreply.github.com>
2021-01-05 17:35:07 +0900

    SocketIO Hello World Part one

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import logger from "morgan";
 import { join } from "path";
 import socketIO from "socket.io";
 
@@ -7,6 +8,7 @@ const app = express();
 
 app.set("view engine", "pug");
 app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
 app.use(express.static(join(__dirname, "static")));
 
 app.get("/", (req, res) => {
@@ -19,3 +21,13 @@ const handleListening = () =>
 const server = app.listen(PORT, handleListening);
 
 const io = socketIO(server);
+
+let sockets = [];
+
+io.on("connection", (socket) => {
+  sockets.push(socket.id);
+});
+
+setInterval(() => {
+  console.log(sockets);
+}, 1000);
