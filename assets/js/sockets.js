7437c292345cde2a3228de2c6a42a59b28e3bcd7
hylas-develop <74232110+hylas-develop@users.noreply.github.com>
2021-01-06 20:03:34 +0900

    Realtime Painting part One

--- assets/js/sockets.js
+++ assets/js/sockets.js
@@ -1,18 +1,18 @@
 import { handleNewMessage } from "./chat";
 import { handleDisconnected, handleNewUser } from "./notifications";
+import { handleBeganPath, handleFilled, handleStrokedPath } from "./paint";
 
 let socket = null;
 // eslint-disable-next-line import/prefer-default-export
 export const getSocket = () => socket;
 
-export const updateSocket = (aSocket) => {
-  socket = aSocket;
-};
-
 export const initSocket = (aSocket) => {
   const { events } = window;
-  updateSocket(aSocket);
-  aSocket.on(events.newUser, handleNewUser);
-  aSocket.on(events.disconnected, handleDisconnected);
-  aSocket.on(events.newMsg, handleNewMessage);
+  socket = aSocket;
+  socket.on(events.newUser, handleNewUser);
+  socket.on(events.disconnected, handleDisconnected);
+  socket.on(events.newMsg, handleNewMessage);
+  socket.on(events.beganPath, handleBeganPath);
+  socket.on(events.strokedPath, handleStrokedPath);
+  socket.on(events.filled, handleFilled);
 };
